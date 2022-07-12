34d4fa7a87915af907fffb5bbbd35cb68ccb4e22
Nayanjyoti Rabha <njrabha6@gmail.com>
2022-07-12 18:58:19 +0530

    new commit

--- src/test/java/request/RequestMethods.java
+++ src/test/java/request/RequestMethods.java
@@ -1,5 +1,7 @@
 package request;
 
+import org.testng.Assert;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -8,18 +10,26 @@ public class RequestMethods {
 
 	public static Response response;
 	public static RequestSpecification responseSpecification = RestAssured.given();
-
-	public void getProduct(String uri, String headers, String parameters, String statusCode, String jsonPath) {
-
+	public static String[] splitParameters;
+	public static String[] splitjsonPaths;
+	public void getProduct(String uri, String headers, String parameters, String statusCode, String jsonPaths) {
+		splitParameters = parameters.split(",");
+		splitjsonPaths = jsonPaths.split(",");
 		response = responseSpecification
 				.when()
-				.queryParam("$limit", 1)
+				.queryParam(splitParameters[0], Integer.parseInt(splitParameters[1]))
 				.get(uri)
 				.then()
 				.log()
 				.all()
 				.extract()
 				.response();
+		String headerValue = response.getHeader("Content-Type");
+		Assert.assertEquals("application/json; charset=utf-8", headerValue);
+		Assert.assertEquals(statusCode, response.statusCode());
+		for(String jsonpath : splitjsonPaths) {
+			Assert.assertEquals(response.jsonPath().getString(jsonpath),"Duracell - AAA Batteries (4-Pack)");
+		}
 	}
 
 	public void postProduct() {
