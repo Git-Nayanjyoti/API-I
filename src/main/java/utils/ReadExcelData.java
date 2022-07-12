11f662fcd74fc31f3c462053f3c464886918a0dd
Nayanjyoti Rabha <njrabha6@gmail.com>
2022-07-12 16:41:08 +0530

    new commit

--- src/main/java/utils/ReadExcelData.java
+++ src/main/java/utils/ReadExcelData.java
@@ -2,7 +2,8 @@ package utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,60 +16,78 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcelData {
-	FileInputStream file;
-	Workbook workbook;
-	Sheet sheet = workbook.getSheetAt(0);
 
-	public ReadExcelData() throws Exception {
-		file = new FileInputStream(new File("./src/test/resources/Book2.xlsx"));
-		workbook  = new XSSFWorkbook(file);
-		// storing the data from excel file to Map
-		Map<Integer, List<String>> data = new HashMap<Integer, List<String>>();
-		int i = 0;
+	public static FileInputStream file;
+	public static String[][] excelData;
+	public ReadExcelData() {
+//	public static void main(String[] args) {
 
-		// Reading the data in
-		for(Row row:sheet){
-			data.put(i, new ArrayList<String>());
-			for (Cell cell : row) {
-				switch (cell.getCellType()) {
-				case STRING:
-					data.get(i).add(cell.getRichStringCellValue().getString());
-					break;
-				case NUMERIC:
-					if (DateUtil.isCellDateFormatted(cell)) {
-						data.get(i).add(cell.getDateCellValue() + "");
-					} else {
-						data.get(i).add(cell.getNumericCellValue() + "");
+		try {
+			file = new FileInputStream(new File("./src/test/resources/TestData.xlsx"));
+			Workbook workbook = new XSSFWorkbook(file);
+			Sheet sheet = workbook.getSheetAt(0);
+
+			// storing the data from excel file to Map
+			int i = 0;
+			int rowNum = sheet.getLastRowNum();
+			int cellNum = sheet.getRow(0).getPhysicalNumberOfCells();
+			excelData = new String[rowNum][cellNum];
+			// Reading the data in
+			for (Row row : sheet) {
+				int j = 0;
+				for (Cell cell : row) {
+					switch (cell.getCellType()) {
+					case STRING:
+						excelData[i][j] = cell.getRichStringCellValue().getString();
+						break;
+					case NUMERIC:
+						if (DateUtil.isCellDateFormatted(cell)) {
+							excelData[i][j] = cell.getDateCellValue() + "";
+						} else {
+							excelData[i][j] = cell.getNumericCellValue() + "";
+						}
+						break;
+					case BOOLEAN:
+						excelData[i][j] = cell.getBooleanCellValue() + "";
+
+						break;
+					case FORMULA:
+						excelData[i][j] = cell.getCellFormula() + "";
+						break;
+
+					default:
 					}
-					break;
-				case BOOLEAN:
-					data.get(i).add(cell.getBooleanCellValue() + "");
-
-					break;
-				case FORMULA:
-					data.get(i).add(cell.getCellFormula() + "");
-					break;
-				default:
-					data.get(i).add(" ");
+					j++;
 				}
+				i++;
 			}
-			i++;
+//			for (int k = 0; k < rowNum; k++) {
+//				for (int l = 0; l < cellNum; l++) {
+//					System.out.print(excelData[k][l] + "\t");
+//				}
+//				System.out.println("\n");
+//			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		// printing the data in tabular form
-		for(int num:data.keySet()){
-			List<String> temp = data.get(num);
-			int count = 0;
-			for (String st : temp) {
-				System.out.print(st + "\t");
-				count++;
-				if (count == temp.size()) {
-					System.out.println("");
-				}
-
-			}
-		}
+//		// printing the data in tabular form
+//		for(int num:data.keySet()){
+//			List<String> temp = data.get(num);
+//			int count = 0;
+//			for (String st : temp) {
+//				System.out.print(st + "\t");
+//				count++;
+//				if (count == temp.size()) {
+//					System.out.println("");
+//				}
+//
+//			}
+//		}
 	}
-
 
 }
--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -1,29 +1,42 @@
 package tests;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.aventstack.extentreports.Status;
-
-import io.restassured.response.Response;
+import utils.ReadExcelData;
 
 public class ProductTest extends BaseTest {
 
+	ReadExcelData readExcelData = new ReadExcelData();
+	
+
+	@SuppressWarnings("static-access")
 	@DataProvider(name = "DataFromExcel")
-	public List<String[]> dataForTest() {
-		List<String[]> list = new ArrayList<String[]>();
-		return list;
+	public Object[][] dataForTest() {
+		return readExcelData.excelData;
 	}
 
-	@Test(dataProvider = "")
-	public void verifyGetRequest() {
-		extentReport.createTestcase("Verify Get Product");
-		Response response =  bestBuyRequestPlant.getAllProducts("/products", "", 0);
-		extentReport.addLog(Status.INFO, response.asPrettyString());
-		response.then().statusCode(200);
+	@Test(dataProvider = "DataFromExcel")
+	public void verifyGetRequest(String data1, String data2, String data3, String data4, String data5, String data6, String data7) {
+		System.out.println("-----");
+		System.out.println("datas");
+		System.out.println(data1);
+		System.out.println("-----");
+//		System.out.println("data2 : ");
+//		System.out.println(data2);
+//		System.out.println("-----");
+//		Assert.assertEquals(data1, "ab");
+//		Assert.assertEquals(data2, "cd");
+//		for(int i =1; i< data.size();i++) {
+//			System.out.println("Data" + i);
+//			System.out.println(data.get(i));
+//		}
+
+//		extentReport.createTestcase("Verify Get Product");
+//		Response response =  bestBuyRequestPlant.getAllProducts("/products", "", 0);
+//		extentReport.addLog(Status.INFO, response.asPrettyString());
+//		response.then().statusCode(200);
+
 	}
 
 }
