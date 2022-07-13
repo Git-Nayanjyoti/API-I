d1971b21032a3966c31a338f77bd87cfd8443bbf
Nayanjyoti Rabha <njrabha6@gmail.com>
2022-07-13 13:35:08 +0530

    new commit

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -14,25 +14,24 @@ import utils.ConfigRead;
 public class BaseTest {
 	String configFilename;
 	Properties configProperties;
-	RequestMethods requestMethods;
 	String currentWorkingDirectory;
-	
+
 	@BeforeSuite
 	public void preSetup() throws IOException {
 		currentWorkingDirectory = System.getProperty("user.dir");
 		configFilename = currentWorkingDirectory + "/src/test/resources/config/config.properties";
 		configProperties = ConfigRead.readConfigProperties(configFilename);
 	}
-	
+
 	@BeforeClass
 	public void setup() {
 		RestAssured.baseURI = configProperties.getProperty("baseUrl");
 		RestAssured.port = Integer.parseInt(configProperties.getProperty("port"));
-		
-		requestMethods = new RequestMethods();
-		
+		System.out.println(configProperties.getProperty("baseUrl"));
+		System.out.println(configProperties.getProperty("port"));
+
 	}
-	
+
 	@AfterClass
 	public void clear() {
 		RestAssured.reset();
--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -1,15 +1,18 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import request.RequestMethods;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import utils.ReadExcelData;
 
 public class ProductTest extends BaseTest {
 
 	ReadExcelData readExcelData = new ReadExcelData();
-	RequestMethods request = new RequestMethods();
 
 	@SuppressWarnings("static-access")
 	@DataProvider(name = "DataFromExcel")
@@ -17,27 +20,138 @@ public class ProductTest extends BaseTest {
 		return readExcelData.excelData;
 	}
 
+	// method for validating Headers
+	public void validateHeaders(Response response, String[] headers) {
+		for (String header : headers) {
+			String[] headerValues = header.split(":");
+			String expected = headerValues[1].replaceAll("\\s", "");
+			String found = response.getHeader(headerValues[0]);
+			Assert.assertEquals(found.replaceAll("\\s", ""), expected);
+		}
+	}
+
+	// method for validating jsonPath
+	public void validateJsonPaths(Response response, String[] jsonPaths) {
+		for (String jsonpath : jsonPaths) {
+			String[] jsonValues = jsonpath.split("=");
+			String expected = jsonValues[0];
+			String found = jsonValues[1];
+			Assert.assertEquals(response.jsonPath().getString(expected), found);
+		}
+	}
+
 	@Test(dataProvider = "DataFromExcel")
 	public void validateRequest(String requestType, String requestURI, String requestHeaders, String requestBody,
 			String requestParameters, String expectedStatusCode, String jsonPath) {
+
+		Response response;
+		RequestSpecification responseSpecification = RestAssured.given().contentType(ContentType.JSON).when();
+		String[] splitParameters;
+		String[] splitjsonPaths;
+		String[] splitStatusCode;
+		String[] splitHeaders;
+
+		int statuscode;
+		int productId;
+
 		switch (requestType) {
 		case "GET":
-			request.getProduct(requestURI, requestHeaders, requestParameters, expectedStatusCode, jsonPath);
+			statuscode = (int) (Math.round(Float.parseFloat(expectedStatusCode)));
+
+			splitHeaders = requestHeaders.split(",");
+			splitParameters = requestParameters.split(",");
+			splitjsonPaths = jsonPath.split(",");
+			response = responseSpecification.queryParam(splitParameters[0], Integer.parseInt(splitParameters[1]))
+					.get(requestURI).then().extract().response();
+
+			// validating jsonpath
+			// calling jsonpath validation function
+			validateJsonPaths(response, splitjsonPaths);
+
+			// validating status code
+			Assert.assertEquals(response.statusCode(), statuscode);
+
+			// validating headers
+			// calling headers validation function
+			validateHeaders(response, splitHeaders);
+
 			break;
 
 		case "POST":
-			request.postProduct(requestURI, requestHeaders, requestBody, expectedStatusCode);
+			statuscode = (int) (Math.round(Float.parseFloat(expectedStatusCode)));
+			splitHeaders = requestHeaders.split(",");
+			response = responseSpecification.body(requestBody).post(requestURI).then().extract().response();
+
+			// validate headers
+			// calling header validation method
+			validateHeaders(response, splitHeaders);
+
+			// validate status code
+			Assert.assertEquals(response.statusCode(), statuscode);
 			break;
 
 		case "PUT":
-			request.putProduct(requestURI, requestHeaders, requestBody, requestParameters, expectedStatusCode);
+			splitHeaders = requestHeaders.split(",");
+			statuscode = (int) (Math.round(Float.parseFloat(expectedStatusCode)));
+
+			// As we need to post something firest in order to put or partially update it
+			// later with the given parameter
+			response = responseSpecification.body(requestBody).post(requestURI).then().extract().response();
+
+			productId = response.path(requestParameters);
+			requestURI = requestURI + "/";
+			int statusCodeOnPut = responseSpecification.body(requestBody).put(requestURI + productId).then().extract()
+					.response().statusCode();
+
+			// validating headeres
+			// calling header validation method
+			validateHeaders(response, splitHeaders);
+
+			// validating status code
+			Assert.assertEquals(statusCodeOnPut, statuscode);
 			break;
 
 		case "PATCH":
-			request.patchProduct(requestURI, requestHeaders, requestBody, requestParameters, expectedStatusCode);
+			statuscode = (int) (Math.round(Float.parseFloat(expectedStatusCode)));
+			splitHeaders = requestHeaders.split(",");
+
+			// As we need to post something firest in order to put or partially update it
+			// later with the given parameter
+			response = responseSpecification.body(requestBody).post(requestURI).then().extract().response();
+
+			productId = response.path(requestParameters);
+			requestURI = requestURI + "/";
+			int statusCodeOnPatch = responseSpecification.body(requestBody).patch(requestURI + productId).then()
+					.extract().response().statusCode();
+
+			// validating headeres
+			// calling header validation method
+			validateHeaders(response, splitHeaders);
+
+			// validating status code
+			Assert.assertEquals(statusCodeOnPatch, statuscode);
 			break;
+			
 		case "DELETE":
-			request.deleteProduct(requestURI, requestHeaders, requestBody, requestParameters, expectedStatusCode);
+			statuscode = (int) (Math.round(Float.parseFloat(expectedStatusCode)));
+			splitHeaders = requestHeaders.split(",");
+			splitStatusCode = expectedStatusCode.split(",");
+			response = responseSpecification.body(requestBody).post(requestURI).then().extract().response();
+			productId = response.path(requestParameters);
+			requestURI = requestURI + "/";
+			int statusCodeOnDelete = responseSpecification.delete(requestURI + productId).then().extract().response()
+					.statusCode();
+
+			// validating headers
+			// calling the header validation method
+			validateHeaders(response, splitHeaders);
+
+			// validating the DELETE post status code
+			Assert.assertEquals(statusCodeOnDelete, statuscode);
+
+			// Also checking it by requesting a get request of the same data and validating
+			// the status code. As it will not be there it will return 400
+//			Assert.assertEquals(Integer.parseInt(splitStatusCode[1]), statusCodeOnGet);
 			break;
 
 		default:
